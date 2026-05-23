965c0b7557b4087b2279eeb90234691ceed1c0ab
ArshiraRosa <olivererz@gmail.com>
2026-05-23 16:21:06 -0400

    Pretty long flower

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pretty_flower
+import modify_flower
 
 if (__name__ == "__main__"):
-    pretty_flower.print_pretty_flower()
+    modify_flower.modify_flower()
 
+pretty_flower.print_pretty_long_flower()
 
--- pretty_flower.py
+++ pretty_flower.py
@@ -4,4 +4,10 @@ def print_pretty_flower():
     print(flower)
     file.close()
 
+
+def print_pretty_long_flower():
+    file = open("pretty_long_flower.txt")
+    flower = file.read()
+    print(flower)
+    file.close()
     
