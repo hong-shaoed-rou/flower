0482c640a5386b4cd205e12ae608e940e33a64b6
hong-shaoed-rou <armanilearns@gmail.com>
2026-05-23 15:19:06 -0400

    adding a pretty flower

---
+++ main.py
@@ -0,0 +1,6 @@
+import pretty_flower
+
+if (__name__ == "__main__"):
+    pretty_flower.print_pretty_flower()
+
+
---
+++ pretty_flower.py
@@ -0,0 +1,7 @@
+def print_pretty_flower():
+    file = open("pretty_flower.txt")
+    flower = file.read()
+    print(flower)
+    file.close()
+
+    
