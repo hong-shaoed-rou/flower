5c85f78454832c2f87179f5dcff2e683289f975a
hong-shaoed-rou <armanilearns@gmail.com>
2026-05-23 15:26:01 -0400

    added many flower function

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ import pretty_flower
 
 if (__name__ == "__main__"):
     pretty_flower.print_pretty_flower()
+    pretty_flower.print_many_flower()
 
 
--- pretty_flower.py
+++ pretty_flower.py
@@ -4,4 +4,8 @@ def print_pretty_flower():
     print(flower)
     file.close()
 
-    
+def print_many_flower():
+    file = open("many_flowers.txt")
+    flower = file.read()
+    print(flower)
+    file.close()
